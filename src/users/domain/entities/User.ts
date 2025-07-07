93497045759f7faa154991520253b247d8999f10
Kevin <moralesperezkevin7@gmail.com>
2025-07-06 23:08:35 -0600

    feat(domain): add user role to User entity

--- src/users/domain/entities/User.ts
+++ src/users/domain/entities/User.ts
@@ -5,10 +5,16 @@ import { UserId } from "../valueObjects/UserId_valueObject";
 import { UserLastName } from "../valueObjects/UserLastName_valueObject";
 import { UserPassword } from "../valueObjects/UserPassword_valueObject";
 
+export enum UserRole {
+  regularUser = 'Regular user',
+  administrator = 'Administrator'
+}
+
 export class User {
 
   constructor(
     private readonly userId: UserId,
+    private readonly userRole: UserRole,
     private readonly firstName: UserFirstName,
     private readonly lastName: UserLastName,
     private readonly email: UserEmail,
@@ -18,6 +24,10 @@ export class User {
 
   getUserId(): string {
     return this.userId.value;
+  }
+
+  getUserRole(): UserRole {
+    return this.userRole;
   }
 
   getFirstName(): string {
